a861d4caf81568de530f395d3aab02bfdbfcfa3f
elyk10 <kweb@rogers.com>
2025-09-13 11:16:00 -0400

    8th project completed

---
+++ eigthProject/main.py
@@ -0,0 +1,18 @@
+def bubbleSort(ls):
+    for i in range(0, len(ls)):
+        for j in range(0, len(ls) - i - 1):
+            if ls[j] > ls[j + 1]:
+                temp = ls[j]
+                ls[j] = ls[j+1]
+                ls[j+1] = temp
+
+    return ls
+
+def main():
+    ls = [3, 2, 44, 5, 9, 1]
+    ls = bubbleSort(ls)
+    print(ls)
+
+main()
+
+
